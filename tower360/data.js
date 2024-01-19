d1cfe691b88889bd84ba019d4c431473577bab52
reihan agam <reihanagam7@gmail.com>
2024-01-19 13:57:10 +0700

    update

---
+++ tower360/data.js
@@ -0,0 +1,61 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-tower-5-360",
+      "name": "tower-5-360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.0695063862093228,
+          "pitch": -0.012633494750556196,
+          "title": "Aiqqon",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.8692563638210977,
+          "pitch": -0.10880671834186728,
+          "title": "Mediffa",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.410345409730411,
+          "pitch": -0.04689643391898102,
+          "title": "DanaSyariah",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Tower360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
